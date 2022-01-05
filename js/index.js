bfcea6c44bdad353ca9efc878e0da0c920b7de33
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2022-01-05 15:34:40 +0300

    #3 Создали главный документ

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const Background = () => {
+    return (
+        <Text>
+            Hello, Nought-Crosses! I`m React Native!
+        </Text>
+    )
+}
+
+export default Background;
