8a56fd21702b4099777d820db29dc31a1ee9b2ee
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2022-01-07 14:07:07 +0300

    #3 Все видно

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-const Background = () => {
-    return (
-        <Text>
-            Hello, Nought-Crosses! I`m React Native!
-        </Text>
-    )
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default Background;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+    backgroundColor: 'rgba(246,248,252,1.0)',
+  },
+  logo: {
+    width: 250,
+    height: 250,
+  },
+});
+
+const SectionListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('/images/icon.png')} />
+    </View>
+  );
+};
+
+export default SectionListBasics;
